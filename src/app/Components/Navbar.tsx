1cb470bd829c85c242987d7c3878ea64b8637c2c
s21sd <srivastavasunny359@gmail.com>
2024-06-30 01:42:28 +0530

    motion added

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -2,29 +2,27 @@
 import { useRouter } from 'next/navigation'
 import React from 'react'
 import { ModeToggle } from './ToggleMode';
-
+import { motion } from 'framer-motion';
+import { transition1 } from './Transition';
+import { FaHeart } from 'react-icons/fa';
 const Navbar = () => {
     const router = useRouter();
     return (
-        <div className="mx-auto flex flex-wrap p-5  flex-col md:flex-row items-center justify-between ">
+        <motion.div initial={{ opacity: 0, y: '-50%' }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: '-50%' }}
+            transition={{ type: 'spring', stiffness: 50, damping: 10 }} className="mx-auto flex flex-wrap p-5  flex-col md:flex-row items-center justify-between ">
             <div onClick={() => router.push('/')} className="flex title-font font-medium items-center mb-4 md:mb-0 ">
                 <h1 className="ml-3 text-2xl tracking-wide cursor-pointer font-bold leading-relaxed text-white mycolor">Bhagavad<span className='text-[#FF8434]'>Gita</span></h1>
             </div>
-            <div>
-                {/* <label
-                    className="relative inline-block h-6 w-12 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-[#FF8434]"
-                >
-                    <input className="peer sr-only" id="AcceptConditions" type="checkbox" />
-                    <span
-                        className="absolute inset-y-0 start-0 m-1 size-4 rounded-full bg-gray-300 ring-[6px] ring-inset ring-white transition-all peer-checked:start-8 peer-checked:w-2 peer-checked:bg-white peer-checked:ring-transparent"
-                    ></span>
-                </label> */}
+            <div className='flex gap-4 items-center'>
+                <FaHeart onClick={() => router.push("/favourites")} className='cursor-pointer' size={36} color='#FF8434' />
                 <ModeToggle />
             </div>
 
 
 
-        </div>
+        </motion.div>
 
     )
 }
